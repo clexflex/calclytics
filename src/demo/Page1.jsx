abd451c4216c62739f3cc99e094674e7e5b07759
yashraj <yashrajjghosalkar@gmail.com>
2025-01-31 12:53:34 +0530

    setup shadcn

--- src/demo/Page1.jsx
+++ src/demo/Page1.jsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
+
 
 const Page1 = () => {
   return (
     <>
+    
       <div class="mt-20">Page1</div>
+      <Button>Click me</Button>
     </>
   );
 };

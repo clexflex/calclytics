eb90b9141ca08d6905b7bb87f80ba6683f8d05b8
yashraj <yashrajjghosalkar@gmail.com>
2025-01-31 10:43:45 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
     <>
-    <div>App</div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <header className="bg-blue-500 text-white p-4">
+        <h1>Welcome to Calclytics</h1>
+      </header>
+      <div>
+        <h2 className="text-2xl">App</h2>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
